9fa80b1682c762fbb35e4f861050b62167844a86
Enmanuel Nava <116982425+end1996@users.noreply.github.com>
2025-01-27 19:41:57 -0500

    Update 10_mixins.dart
    Actualización sobre implementación de mixin, cambio de abstract class Volador, Caminante. Nadador por la palabra revervada directamente mixin

--- 10_mixins.dart
+++ 10_mixins.dart
@@ -4,15 +4,15 @@ abstract class Mamifero extends Animal {}
 abstract class Ave extends Animal {}
 abstract class Pez extends Animal {}
 
-abstract class Volador {
+mixin Volador {
   void volar() => print('estoy volando!');
 }
 
-abstract class Caminante {
+mixin Caminante {
   void caminar() => print('estoy caminando!');
 }
 
-abstract class Nadador {
+mixin Nadador {
   void nadar() => print('estoy nadando!');
 }
 
